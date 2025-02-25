1b799df794bb82012d018069885f3ca91127182c
megha-r20 <megha202005@gmail.com>
2025-02-25 14:39:01 +0530

    fgh

---
+++ frontend/src/Page/LoginPage.jsx
@@ -0,0 +1,16 @@
+
+import Login from '../Components/Login/Login'
+
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>
+      <Login/>
+      </h1>
+    </div>
+  )
+}
+
+export default LoginPage
